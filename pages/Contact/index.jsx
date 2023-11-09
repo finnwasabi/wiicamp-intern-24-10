b9223bbba6a7ea5e847ae201ee0d79415f91a928
Tùng Finn Wasabi <you@example.com>
2023-11-09 15:19:22 +0700

    test

--- pages/Contact/index.jsx
+++ pages/Contact/index.jsx
@@ -1,44 +1,52 @@
-import React, { useEffect, useState } from "react";
-import Head from "next/head";
+// import React, { useEffect, useState } from "react";
+// import Head from "next/head";
 
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import ContactSection from "@/components/Sections/ContactSection";
-import TopHeader from "@/components/TopHeader";
+// import Footer from "@/components/Footer";
+// import Header from "@/components/Header";
+// import ContactSection from "@/components/Sections/ContactSection";
+// import TopHeader from "@/components/TopHeader";
 
-function Contact() {
-  const [isScrolled, setIsScrolled] = useState(false);
+// function Contact() {
+//   const [isScrolled, setIsScrolled] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollY = window.scrollY;
-      if (currentScrollY > 0) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
-    };
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       const currentScrollY = window.scrollY;
+//       if (currentScrollY > 0) {
+//         setIsScrolled(true);
+//       } else {
+//         setIsScrolled(false);
+//       }
+//     };
 
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-  return (
-    <>
-      <Head>
-        <title>Contact</title>
-      </Head>
-      <main>
-        <TopHeader />
-        <div className={isScrolled ? "fixed -top-[23px] z-50 w-full" : ""}>
-          <Header show3icons />
-        </div>
-        <div className="mt-[5.9375rem]">
-          <ContactSection />
-          <Footer />
-        </div>
-      </main>
-    </>
-  );
+//     window.addEventListener("scroll", handleScroll);
+//     return () => window.removeEventListener("scroll", handleScroll);
+//   }, []);
+//   return (
+//     <>
+//       <Head>
+//         <title>Contact</title>
+//       </Head>
+//       <main>
+//         <TopHeader />
+//         <div className={isScrolled ? "fixed -top-[23px] z-50 w-full" : ""}>
+//           <Header show3icons />
+//         </div>
+//         <div className="mt-[5.9375rem]">
+//           <ContactSection />
+//           <Footer />
+//         </div>
+//       </main>
+//     </>
+//   );
+// }
+
+// export default Contact;
+
+import React from "react";
+
+function index() {
+  return <div>index</div>;
 }
 
-export default Contact;
+export default index;

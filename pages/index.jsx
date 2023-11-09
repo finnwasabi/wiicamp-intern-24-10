5d080356753cb3c8a9ec45f911096f75cbf6efd6
Tùng Finn Wasabi <you@example.com>
2023-11-09 10:45:59 +0700

    tiep

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,19 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import PropTypes from "prop-types";
 
 import FillUpArrow from "@/components/Buttons/FillUpArrow";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Categories from "@/components/Sections/Categories";
 import CustomerServices from "@/components/Sections/CustomerServices";
+import FlashSales from "@/components/Sections/FlashSales";
 import JBLFlashSale from "@/components/Sections/JBLFlashSale";
 import NewArrival from "@/components/Sections/NewArrival";
+import OurProducts from "@/components/Sections/OurProducts";
+import ThisMonth from "@/components/Sections/ThisMonth";
 import TopSection from "@/components/Sections/TopSection";
 import TopHeader from "@/components/TopHeader";
 
@@ -27,7 +32,7 @@ export async function getServerSideProps() {
   }
 }
 
-export default function Home() {
+export default function Home({ products }) {
   const [isScrolled, setIsScrolled] = useState(false);
   useEffect(() => {
     const handleScroll = () => {
@@ -49,26 +54,26 @@ export default function Home() {
       <div className={isScrolled ? "fixed -top-[23px] z-50 w-full" : ""}>
         <Header show3icons />
       </div>
-      <div className="mt-[5.9375rem]">
+      {/* <div className="mt-[5.9375rem]">
         <TopSection />
-        {/* <FlashSales products={products} /> */}
+        <FlashSales products={products} />
         <Categories />
-        {/* <ThisMonth products={products} /> */}
+        <ThisMonth products={products} />
         <JBLFlashSale />
-        {/* <OurProducts products={products} /> */}
+        <OurProducts products={products} />
         <NewArrival />
         <CustomerServices />
         <FillUpArrow />
         <Footer />
-      </div>
+      </div> */}
     </main>
   );
 }
 
-// Home.propTypes = {
-//   products: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       // Define the shape of the array elements here
-//     }),
-//   ).isRequired,
-// };
+Home.propTypes = {
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      // Define the shape of the array elements here
+    }),
+  ).isRequired,
+};

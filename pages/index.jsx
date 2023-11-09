edf4b0b36fe644e71c639a57e08c2eeb0a784b92
Tùng Finn Wasabi <you@example.com>
2023-11-09 10:02:03 +0700

    fix 500

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 import PropTypes from "prop-types";
 
 import FillUpArrow from "@/components/Buttons/FillUpArrow";
@@ -15,11 +16,9 @@ import ThisMonth from "@/components/Sections/ThisMonth";
 import TopSection from "@/components/Sections/TopSection";
 import TopHeader from "@/components/TopHeader";
 
-import request from "./request";
-
 export async function getServerSideProps() {
   try {
-    const response = await request.get("products");
+    const response = await axios.get("https://fakestoreapi.com/products");
     const products = response.data;
     return {
       props: { products },

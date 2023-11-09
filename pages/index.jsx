068d306288090135f5beb21e6b81522fdfb0421b
Tùng Finn Wasabi <you@example.com>
2023-11-09 10:26:54 +0700

    fix 500

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,19 +1,19 @@
 /* eslint-disable no-console */
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
-import FillUpArrow from "@/components/Buttons/FillUpArrow";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import Categories from "@/components/Sections/Categories";
-import CustomerServices from "@/components/Sections/CustomerServices";
-import FlashSales from "@/components/Sections/FlashSales";
-import JBLFlashSale from "@/components/Sections/JBLFlashSale";
-import NewArrival from "@/components/Sections/NewArrival";
-import OurProducts from "@/components/Sections/OurProducts";
-import ThisMonth from "@/components/Sections/ThisMonth";
-import TopSection from "@/components/Sections/TopSection";
-import TopHeader from "@/components/TopHeader";
+// import FillUpArrow from "@/components/Buttons/FillUpArrow";
+// import Footer from "@/components/Footer";
+// import Header from "@/components/Header";
+// import Categories from "@/components/Sections/Categories";
+// import CustomerServices from "@/components/Sections/CustomerServices";
+// import FlashSales from "@/components/Sections/FlashSales";
+// import JBLFlashSale from "@/components/Sections/JBLFlashSale";
+// import NewArrival from "@/components/Sections/NewArrival";
+// import OurProducts from "@/components/Sections/OurProducts";
+// import ThisMonth from "@/components/Sections/ThisMonth";
+// import TopSection from "@/components/Sections/TopSection";
+// import TopHeader from "@/components/TopHeader";
 
 export async function getServerSideProps() {
   try {
@@ -30,42 +30,43 @@ export async function getServerSideProps() {
   }
 }
 
-export default function Home({ products }) {
-  const [isScrolled, setIsScrolled] = useState(false);
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollY = window.scrollY;
-      if (currentScrollY > 0) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
-    };
+export default function Home() {
+  // const [isScrolled, setIsScrolled] = useState(false);
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     const currentScrollY = window.scrollY;
+  //     if (currentScrollY > 0) {
+  //       setIsScrolled(true);
+  //     } else {
+  //       setIsScrolled(false);
+  //     }
+  //   };
 
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+  //   window.addEventListener("scroll", handleScroll);
+  //   return () => window.removeEventListener("scroll", handleScroll);
+  // }, []);
 
-  return (
-    <main className="overflow-x-hidden">
-      <TopHeader />
-      <div className={isScrolled ? "fixed -top-[23px] z-50 w-full" : ""}>
-        <Header show3icons />
-      </div>
-      <div className="mt-[5.9375rem]">
-        <TopSection />
-        <FlashSales products={products} />
-        <Categories />
-        <ThisMonth products={products} />
-        <JBLFlashSale />
-        <OurProducts products={products} />
-        <NewArrival />
-        <CustomerServices />
-        <FillUpArrow />
-        <Footer />
-      </div>
-    </main>
-  );
+  return "Helo";
+  // return (
+  //   <main className="overflow-x-hidden">
+  //     <TopHeader />
+  //     <div className={isScrolled ? "fixed -top-[23px] z-50 w-full" : ""}>
+  //       <Header show3icons />
+  //     </div>
+  //     <div className="mt-[5.9375rem]">
+  //       <TopSection />
+  //       <FlashSales products={products} />
+  //       <Categories />
+  //       <ThisMonth products={products} />
+  //       <JBLFlashSale />
+  //       <OurProducts products={products} />
+  //       <NewArrival />
+  //       <CustomerServices />
+  //       <FillUpArrow />
+  //       <Footer />
+  //     </div>
+  //   </main>
+  // );
 }
 
 Home.propTypes = {

3ea5f5267a077afdca85d8da01d1b8ddf9883687
Tùng Finn Wasabi <you@example.com>
2023-11-09 09:18:37 +0700

    fix 500

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,51 @@
+/* eslint-disable */
 import React from "react";
 import { Poppins } from "next/font/google";
 import PropTypes from "prop-types";
 
 import "@/styles/globals.css";
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // Define a state variable to track whether is an error or not
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI
+
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can use your own error logging service here
+    console.log({ error, errorInfo });
+  }
+
+  render() {
+    // Check if the error is thrown
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return (
+        <div>
+          <h2>Oops, there is an error!</h2>
+          <button
+            type="button"
+            onClick={() => this.setState({ hasError: false })}
+          >
+            Try again?
+          </button>
+        </div>
+      );
+    }
+
+    // Return children components in case of no error
+
+    return this.props.children;
+  }
+}
 
 const poppins = Poppins({
   weight: "400",
@@ -11,7 +54,9 @@ const poppins = Poppins({
 export default function App({ Component, pageProps }) {
   return (
     <div className={poppins.className}>
-      <Component {...pageProps} />
+      <ErrorBoundary>
+        <Component {...pageProps} />
+      </ErrorBoundary>
     </div>
   );
 }

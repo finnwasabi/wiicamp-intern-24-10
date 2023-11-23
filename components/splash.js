9a81a909fdca7828d53b2e56a12aecec816bdcea
Tùng Finn Wasabi <you@example.com>
2023-11-23 11:59:18 +0700

    change image tags

--- components/splash.js
+++ components/splash.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Image from "next/image";
 
 function Splash() {
   return (
     <div className="splash-container">
       {/* Your splash image or loading animation */}
-      <img src="splash-image.png" alt="Loading..." />
+      <Image src="splash-image.png" alt="Loading..." />
       {/* You can add a loading message if needed */}
       <p>Loading...</p>
     </div>

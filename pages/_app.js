d917e0a31834b558704ee3bdfc2abb8cd3d945c8
Tùng Finn Wasabi <you@example.com>
2023-11-24 09:17:08 +0700

    test 2

--- pages/_app.js
+++ pages/_app.js
@@ -66,52 +66,6 @@ export default function App({ Component, pageProps }) {
     <CartProvider>
       <NextNProgress color="#DB4444" height={2} />
       <Head>
-        <title>FinnWasabi`&apos;`s Intern E-Commerce Project at WiiCamp</title>
-        <meta
-          name="title"
-          content="FinnWasabi's Intern E-Commerce Project at WiiCamp"
-        />
-        <meta
-          name="description"
-          content="Le Nhat Tung aka Finn Wasabi's Intern Project at WiiCamp"
-        />
-
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:url"
-          content="https://wiicamp-intern-24-10-tung.vercel.app/"
-        />
-        <meta
-          property="og:title"
-          content="FinnWasabi's Intern E-Commerce Project at WiiCamp"
-        />
-        <meta
-          property="og:description"
-          content="Le Nhat Tung aka Finn Wasabi's Intern Project at WiiCamp"
-        />
-        <meta
-          property="og:image"
-          content="https://wiicamp-intern-24-10-tung.vercel.app/FinnWasabi.jpg"
-        />
-
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta
-          property="twitter:url"
-          content="https://wiicamp-intern-24-10-tung.vercel.app/"
-        />
-        <meta
-          property="twitter:title"
-          content="FinnWasabi's Intern E-Commerce Project at WiiCamp"
-        />
-        <meta
-          property="twitter:description"
-          content="Le Nhat Tung aka Finn Wasabi's Intern Project at WiiCamp"
-        />
-        <meta
-          property="twitter:image"
-          content="https://wiicamp-intern-24-10-tung.vercel.app/FinnWasabi.jpg"
-        />
-
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#000" />
         <meta name="apple-mobile-web-app-status-bar" content="#DB4444" />
--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,49 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="title"
+          content="FinnWasabi's Intern E-Commerce Project at WiiCamp"
+        />
+        <meta
+          name="description"
+          content="Le Nhat Tung aka Finn Wasabi's Intern Project at WiiCamp"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://wiicamp-intern-24-10-tung.vercel.app/"
+        />
+        <meta
+          property="og:title"
+          content="FinnWasabi's Intern E-Commerce Project at WiiCamp"
+        />
+        <meta
+          property="og:description"
+          content="Le Nhat Tung aka Finn Wasabi's Intern Project at WiiCamp"
+        />
+        <meta property="og:image" content="/FinnWasabi.jpg" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://wiicamp-intern-24-10-tung.vercel.app/"
+        />
+        <meta
+          property="twitter:title"
+          content="FinnWasabi's Intern E-Commerce Project at WiiCamp"
+        />
+        <meta
+          property="twitter:description"
+          content="Le Nhat Tung aka Finn Wasabi's Intern Project at WiiCamp"
+        />
+        <meta
+          property="twitter:image"
+          content="https://wiicamp-intern-24-10-tung.vercel.app/FinnWasabi.jpg"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />

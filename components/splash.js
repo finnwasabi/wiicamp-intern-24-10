a8e047467ccf610ca5b3f4b4327509aff2d10dd3
Tùng Finn Wasabi <you@example.com>
2023-11-24 09:32:29 +0700

    update counter

--- components/splash.js
+++ components/splash.js
@@ -1,17 +1,24 @@
 import React from "react";
+import Head from "next/head";
 import Image from "next/image";
 
 function Splash() {
   return (
-    <div className="splash-container container flex h-screen items-center justify-center">
-      <Image
-        className="animate-bounce"
-        src="/splash-image.png"
-        width={150}
-        height={150}
-        alt="Loading..."
-      />
-    </div>
+    <>
+      <Head>
+        <title>Loading...</title>
+        <meta name="description" content="Loading..." />
+      </Head>
+      <div className="splash-container container flex h-screen items-center justify-center">
+        <Image
+          className="animate-bounce"
+          src="/splash-image.png"
+          width={150}
+          height={150}
+          alt="Loading..."
+        />
+      </div>
+    </>
   );
 }
 

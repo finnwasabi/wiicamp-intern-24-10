114ce3e6181e33ff326d6143fa00f069cf660696
Tùng Finn Wasabi <you@example.com>
2023-11-24 13:52:53 +0700

    fix no splash screen for SEO

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ import PropTypes from "prop-types";
 
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import Splash from "@/components/splash";
+// import Splash from "@/components/splash";
 import TopHeader from "@/components/TopHeader";
 
 import { CartProvider } from "@/contexts/CartContext";
@@ -23,15 +23,15 @@ const poppins = Poppins({
 });
 export default function App({ Component, pageProps }) {
   const { login, setInitApp } = useAuthStore();
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setLoading(false);
-    }, 3000);
+  // useEffect(() => {
+  //   const timeout = setTimeout(() => {
+  //     setLoading(false);
+  //   }, 3000);
 
-    return () => clearTimeout(timeout);
-  }, []);
+  //   return () => clearTimeout(timeout);
+  // }, []);
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -58,138 +58,138 @@ export default function App({ Component, pageProps }) {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  if (loading) {
-    return (
-      <>
-        <Splash />
-        <div className="hidden">
-          <NextNProgress color="#DB4444" height={2} />
-          <Head>
-            <link rel="manifest" href="/manifest.json" />
-            <meta name="theme-color" content="#000" />
-            <meta name="apple-mobile-web-app-status-bar" content="#DB4444" />
-            <link
-              rel="apple-touch-icon-precomposed"
-              sizes="57x57"
-              href="https://wiicamp-intern-24-10-tung.vercel.app/apple-touch-icon-57x57.png"
-            />
-            <link
-              rel="apple-touch-icon-precomposed"
-              sizes="114x114"
-              href="https://wiicamp-intern-24-10-tung.vercel.app/apple-touch-icon-114x114.png"
-            />
-            <link
-              rel="apple-touch-icon-precomposed"
-              sizes="72x72"
-              href="https://wiicamp-intern-24-10-tung.vercel.app/apple-touch-icon-72x72.png"
-            />
-            <link
-              rel="apple-touch-icon-precomposed"
-              sizes="144x144"
-              href="https://wiicamp-intern-24-10-tung.vercel.app/apple-touch-icon-144x144.png"
-            />
-            <link
-              rel="apple-touch-icon-precomposed"
-              sizes="60x60"
-              href="https://wiicamp-intern-24-10-tung.vercel.app/apple-touch-icon-60x60.png"
-            />
-            <link
-              rel="apple-touch-icon-precomposed"
-              sizes="120x120"
-              href="https://wiicamp-intern-24-10-tung.vercel.app/apple-touch-icon-120x120.png"
-            />
-            <link
-              rel="apple-touch-icon-precomposed"
-              sizes="76x76"
-              href="https://wiicamp-intern-24-10-tung.vercel.app/apple-touch-icon-76x76.png"
-            />
-            <link
-              rel="apple-touch-icon-precomposed"
-              sizes="152x152"
-              href="https://wiicamp-intern-24-10-tung.vercel.app/apple-touch-icon-152x152.png"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              href="https://wiicamp-intern-24-10-tung.vercel.app/favicon-196x196.png"
-              sizes="196x196"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              href="https://wiicamp-intern-24-10-tung.vercel.app/favicon-96x96.png"
-              sizes="96x96"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              href="https://wiicamp-intern-24-10-tung.vercel.app/favicon-32x32.png"
-              sizes="32x32"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              href="https://wiicamp-intern-24-10-tung.vercel.app/favicon-16x16.png"
-              sizes="16x16"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              href="https://wiicamp-intern-24-10-tung.vercel.app/favicon-128.png"
-              sizes="128x128"
-            />
-            <meta
-              name="application-name"
-              content="FinnWasabi's E-Commerce Project"
-            />
-            <meta name="msapplication-TileColor" content="#FFFFFF" />
-            <meta
-              name="msapplication-TileImage"
-              content="https://wiicamp-intern-24-10-tung.vercel.app/mstile-144x144.png"
-            />
-            <meta
-              name="msapplication-square70x70logo"
-              content="https://wiicamp-intern-24-10-tung.vercel.app/mstile-70x70.png"
-            />
-            <meta
-              name="msapplication-square150x150logo"
-              content="https://wiicamp-intern-24-10-tung.vercel.app/mstile-150x150.png"
-            />
-            <meta
-              name="msapplication-wide310x150logo"
-              content="https://wiicamp-intern-24-10-tung.vercel.app/mstile-310x150.png"
-            />
-            <meta
-              name="msapplication-square310x310logo"
-              content="https://wiicamp-intern-24-10-tung.vercel.app/mstile-310x310.png"
-            />
-          </Head>
-          <div
-            className={clsx(poppins.className, "flex min-h-screen flex-col")}
-          >
-            <TopHeader />
-            <Header isScrolled={isScrolled} />
-            <ToastContainer
-              position="top-right"
-              autoClose={2000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              theme="colored"
-            />
-            <div className="grow">
-              <Component {...pageProps} />
-            </div>
-            <Footer />
-          </div>
-        </div>
-      </>
-    );
-  }
+  // if (loading) {
+  //   return (
+  //     <>
+  //       <Splash />
+  //       <div className="hidden">
+  //         <NextNProgress color="#DB4444" height={2} />
+  //         <Head>
+  //           <link rel="manifest" href="/manifest.json" />
+  //           <meta name="theme-color" content="#000" />
+  //           <meta name="apple-mobile-web-app-status-bar" content="#DB4444" />
+  //           <link
+  //             rel="apple-touch-icon-precomposed"
+  //             sizes="57x57"
+  //             href="https://wiicamp-intern-24-10-tung.vercel.app/apple-touch-icon-57x57.png"
+  //           />
+  //           <link
+  //             rel="apple-touch-icon-precomposed"
+  //             sizes="114x114"
+  //             href="https://wiicamp-intern-24-10-tung.vercel.app/apple-touch-icon-114x114.png"
+  //           />
+  //           <link
+  //             rel="apple-touch-icon-precomposed"
+  //             sizes="72x72"
+  //             href="https://wiicamp-intern-24-10-tung.vercel.app/apple-touch-icon-72x72.png"
+  //           />
+  //           <link
+  //             rel="apple-touch-icon-precomposed"
+  //             sizes="144x144"
+  //             href="https://wiicamp-intern-24-10-tung.vercel.app/apple-touch-icon-144x144.png"
+  //           />
+  //           <link
+  //             rel="apple-touch-icon-precomposed"
+  //             sizes="60x60"
+  //             href="https://wiicamp-intern-24-10-tung.vercel.app/apple-touch-icon-60x60.png"
+  //           />
+  //           <link
+  //             rel="apple-touch-icon-precomposed"
+  //             sizes="120x120"
+  //             href="https://wiicamp-intern-24-10-tung.vercel.app/apple-touch-icon-120x120.png"
+  //           />
+  //           <link
+  //             rel="apple-touch-icon-precomposed"
+  //             sizes="76x76"
+  //             href="https://wiicamp-intern-24-10-tung.vercel.app/apple-touch-icon-76x76.png"
+  //           />
+  //           <link
+  //             rel="apple-touch-icon-precomposed"
+  //             sizes="152x152"
+  //             href="https://wiicamp-intern-24-10-tung.vercel.app/apple-touch-icon-152x152.png"
+  //           />
+  //           <link
+  //             rel="icon"
+  //             type="image/png"
+  //             href="https://wiicamp-intern-24-10-tung.vercel.app/favicon-196x196.png"
+  //             sizes="196x196"
+  //           />
+  //           <link
+  //             rel="icon"
+  //             type="image/png"
+  //             href="https://wiicamp-intern-24-10-tung.vercel.app/favicon-96x96.png"
+  //             sizes="96x96"
+  //           />
+  //           <link
+  //             rel="icon"
+  //             type="image/png"
+  //             href="https://wiicamp-intern-24-10-tung.vercel.app/favicon-32x32.png"
+  //             sizes="32x32"
+  //           />
+  //           <link
+  //             rel="icon"
+  //             type="image/png"
+  //             href="https://wiicamp-intern-24-10-tung.vercel.app/favicon-16x16.png"
+  //             sizes="16x16"
+  //           />
+  //           <link
+  //             rel="icon"
+  //             type="image/png"
+  //             href="https://wiicamp-intern-24-10-tung.vercel.app/favicon-128.png"
+  //             sizes="128x128"
+  //           />
+  //           <meta
+  //             name="application-name"
+  //             content="FinnWasabi's E-Commerce Project"
+  //           />
+  //           <meta name="msapplication-TileColor" content="#FFFFFF" />
+  //           <meta
+  //             name="msapplication-TileImage"
+  //             content="https://wiicamp-intern-24-10-tung.vercel.app/mstile-144x144.png"
+  //           />
+  //           <meta
+  //             name="msapplication-square70x70logo"
+  //             content="https://wiicamp-intern-24-10-tung.vercel.app/mstile-70x70.png"
+  //           />
+  //           <meta
+  //             name="msapplication-square150x150logo"
+  //             content="https://wiicamp-intern-24-10-tung.vercel.app/mstile-150x150.png"
+  //           />
+  //           <meta
+  //             name="msapplication-wide310x150logo"
+  //             content="https://wiicamp-intern-24-10-tung.vercel.app/mstile-310x150.png"
+  //           />
+  //           <meta
+  //             name="msapplication-square310x310logo"
+  //             content="https://wiicamp-intern-24-10-tung.vercel.app/mstile-310x310.png"
+  //           />
+  //         </Head>
+  //         <div
+  //           className={clsx(poppins.className, "flex min-h-screen flex-col")}
+  //         >
+  //           <TopHeader />
+  //           <Header isScrolled={isScrolled} />
+  //           <ToastContainer
+  //             position="top-right"
+  //             autoClose={2000}
+  //             hideProgressBar={false}
+  //             newestOnTop={false}
+  //             closeOnClick
+  //             rtl={false}
+  //             pauseOnFocusLoss
+  //             draggable
+  //             pauseOnHover
+  //             theme="colored"
+  //           />
+  //           <div className="grow">
+  //             <Component {...pageProps} />
+  //           </div>
+  //           <Footer />
+  //         </div>
+  //       </div>
+  //     </>
+  //   );
+  // }
 
   return (
     <CartProvider>
